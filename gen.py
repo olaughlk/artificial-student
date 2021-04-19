077e3aa81ab59cb04edd7953ea8a5e56a3efcdf4
kadeolaughlin <55366417+kadeolaughlin@users.noreply.github.com>
2021-04-19 09:20:12 -0400

    will now generate 100 tweet length characters

--- gen.py
+++ gen.py
@@ -186,11 +186,14 @@ states = None
 next_char = tf.constant(['GVSU'])
 result = [next_char]
 
-for n in range(1,1000):
-    next_char, states = one_step_model.generate_one_step(inputs=next_char, states=states)
-    result.append(next_char)
+for i in range(100):
+    for n in range(1,280):
+        next_char, states = one_step_model.generate_one_step(inputs=next_char, states=states)
+        result.append(next_char)
 
-result = tf.strings.join(result)
-end = time.time()
-print(result[0].numpy().decode('utf-8'), '\n\n' + '_'*80)
+    result = tf.strings.join(result)
+    end = time.time()
+    print(result[0].numpy().decode('utf-8'), '\n\n' + '_'*80)
+    print()
+
 print('\nRun time: ', end - start)
